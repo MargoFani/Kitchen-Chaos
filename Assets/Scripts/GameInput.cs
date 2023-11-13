80f9e3ea6150b72649260f2426d10ba2130289b1
Margo Fanina <margofani.un.go@gmail.com>
2023-11-13 17:02:54 +0300

    add interact events

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameInput : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
+    public event EventHandler OnInteractAction;
+
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+        playerInputActions.Player.Interact.performed += Interact_perfomed;
+    }
+
+    private void Interact_perfomed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+         OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
     public Vector2 GetMovementVectorNormalized()
     {
