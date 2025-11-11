57f5565481302fe1a0a233d73d47f24715a40abc
Margo Fanina <margofani.un.go@gmail.com>
2025-11-11 09:37:19 +0300

    clear counter add

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,11 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
+
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
     private void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
@@ -17,29 +20,13 @@ public class Player : MonoBehaviour
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero)
-        {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                clearCounter.Interact();
-            }
-        }
+        selectedCounter?.Interact();
     }
 
     private void Update()
     {
         HandleMovement();
-        //HandleInteractions();
+        HandleInteractions();
     }
 
     public bool IsWalking()
@@ -60,11 +47,18 @@ public class Player : MonoBehaviour
         float interactDistance = 2f;        
         if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
         {
-            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-                clearCounter.Interact();
+                //Has ClearCounter
+                if (clearCounter != selectedCounter)
+                    selectedCounter = clearCounter;
+
             }
+            else selectedCounter = null;
         }
+        else selectedCounter = null;
+
+        Debug.Log(selectedCounter);
     }
 
     private void HandleMovement()
