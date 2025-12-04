76c2cf64e4870445efa850b9cee8eef37d1e8cd6
Margo Fanina <margofani.un.go@gmail.com>
2025-12-04 10:31:48 +0300

    add ui at the plate with ingridients icons

---
+++ Assets/Scripts/PlateIconsUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PlateKitchenObject;
+
+public class PlateIconsUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+
+        plateKitchenObject.OnIngridientAdded += PlateKitchenObject_OnIngridientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngridientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        UpdateVisual(e.kitchenObjectSO);
+    }
+
+    private void UpdateVisual(KitchenObjectSO kitchenObjectSO)
+    {
+/*        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }*/
+        //из туториала
+        //foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        //{
+        //    Transform iconTransform = Instantiate(iconTemplate, transform);
+        //    iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+        //}
+
+        Transform iconTransform = Instantiate(iconTemplate, transform);
+        iconTransform.gameObject.SetActive(true);
+        iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+    }
+}
